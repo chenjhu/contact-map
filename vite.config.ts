0cfb32b3636c006b4da831c6c06628749ee54ee3
chenjhu <letsmailhhu@gmail.com>
2026-01-19 00:32:21 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
+    base: '/contact-map/',
     const env = loadEnv(mode, '.', '');
     return {
       server: {
